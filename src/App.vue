ce2521248097e136ed2459cce383f31cff6ef0aa
Humeira Diljore <hello@echdee.me>
2019-03-18 21:43:19 +0400

    fine tuning landing page

--- src/App.vue
+++ src/App.vue
@@ -5,93 +5,88 @@
         <div class="container has-text-centered">
           <div class="columns is-centered is-vcentered">
             <div class="column is-two-fifth white-seperator">
-              <h1 class="custom-subtitle">Mauritius, June 10</h1>
+              <h1 class="custom-subtitle">Mauritius, 31st March & 1st June 2019</h1>
               <h2 class="custom-title">Switch Conference</h2>
             </div>
             <div class="column is-two-fifth">
               <img class="logo-hero" :src="require('./assets/logo.png')">
             </div>
-          </div> <!-- Columns End -->
-          <button class="button custom-button">
-              Call For Sponsors
-          </button>
+          </div>
+          <!-- Columns End -->
+          <a class="button is-large custom-button">Call For Sponsors</a>
         </div>
       </div>
     </section>
-    <!-- <div class="hero">
-      <h1>
-        Switch Conference
-        31st March & 1st June 2019
-        Mauritius 🇲🇺
-      </h1>
-      <a href class="button">Call for sponsors</a>
-    </div>-->
   </div>
 </template>
 
 <style lang="scss" scoped>
+$mobile: 768px;
 
-$mobile:400px;
+.white-seperator {
+  border-right: 4px solid white;
 
-.white-seperator{
-    border-right: 4px solid white;
-
-    @media only screen and (max-width:$mobile) {
+  @media only screen and (max-width: $mobile) {
     border: none;
   }
 }
 
 .mainhero {
   background: url("./assets/img1.png") no-repeat fixed center;
+  height: 100vh;
+  display: flex;
+  align-content: center;
+  justify-content: center;
 
   .hero-body {
     background: linear-gradient(180deg, rgba(39, 65, 143, 0.6) 0%, rgba(205, 93, 86, 0.6) 100%);
   }
 }
 
-.custom-button{
-  margin-top:1rem;
+.custom-button {
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  background: #27418F;
-  border: 0px;
+  background: #27418f;
+  border: 4px solid #27418f;
   border-radius: 4px;
   font-size: 1.5rem;
-  color: white;
-  font-family:"Ubuntu",sans-serif;
+  color:#ffffff;
+  transition: all ease-in 0.2s;
+
+  &:hover {
+    background: transparent;
+    color:#ffffff;
+    border-color: #27418f;
+  }
 }
 
-.custom-title{
-font-family: Ubuntu;
-font-weight: bold;
-font-size: 6rem;
-text-align: center;
-color: #FFFFFF;
-  @media only screen and (max-width:$mobile) {
+.custom-title {
+  font-family: Lato;
+  font-weight: bold;
+  font-size: 48px;
+  text-align: center;
+  color: #ffffff;
+  line-height: normal;
+  @media only screen and (max-width: $mobile) {
     font-size: 3rem;
   }
 }
 
-.custom-subtitle{
-  text-align:center;
-  font-family: Lato;
-font-style: normal;
-font-weight: normal;
-font-size: 36px;
-line-height: normal;
-color: #FFFFFF;
+.custom-subtitle {
+  text-align: center;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 24px;
+  line-height: normal;
+  color: #ffffff;
 
-  @media only screen and (max-width:$mobile) {
+  @media only screen and (max-width: $mobile) {
     font-size: 1.5rem;
   }
 }
 
 .logo-hero {
-  max-height:50%;
-  max-width:50%;
-
-  @media only screen and (max-width:$mobile) {
-    margin-left: 4rem;
-  }
+  max-height: 50%;
+  max-width: 50%;
 }
 </style>
 
